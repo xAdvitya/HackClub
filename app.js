99b764dc25e4c3b1b812ff739ee2195d9fd323fe
Advitya sharma <advityasharma21@gmail.com>
2020-03-02 19:53:11 +0530

    using express.use to fetch static files

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ var app;
 
 app = express();
 
+app.use(express.static('public'));
+
+// manually sending files
+/*
 app.get('/',function(req,res){
-    res.send("response is send");
+
+    res.sendFile(__dirname+'/index.html');
 });
 
+app.get('/style.css',function(req,res){
+    res.sendFile(__dirname+'/style.css');
+});
+*/
 port = 5001
 
 app.listen(port,function(){
